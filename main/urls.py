1c278bc73de4dc7847b99a36842cea3cfa15337e
symtalha14 <syedtalha5314@gmail.com>
2022-01-04 02:00:11 +0530

    fasd

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import action, addRecord, showRecords
+from .views import action, addRecord, privacy, showRecords
 
 
 urlpatterns=[
     path('command/', action, name="command_action" ),
     path('record/add/', addRecord, name="add_record"),
     path('record/show/', showRecords, name="show_records"),
-    path("privacy-policy/")
+    path("privacy-policy/", privacy)
 ]
